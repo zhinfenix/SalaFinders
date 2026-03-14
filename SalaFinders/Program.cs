869486f460e0ee24fadad426e4c1e5ac3d48ed7b
José David Guzmán Gutiérrez <jose.guzman33@eia.edu.co>
2026-03-14 10:42:27 -0500

    swagger funciona

--- SalaFinders/Program.cs
+++ SalaFinders/Program.cs
@@ -3,7 +3,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+// Agrega estas líneas para que el proyecto reconozca Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -11,13 +13,15 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    // Esto activa la generación del archivo JSON y la interfaz visual
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
+    // Si quieres conservar la nueva forma de .NET 9, puedes dejar esta:
     app.MapOpenApi();
 }
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
-app.MapControllers();
+app.MapControllers(); // ¡Esta es vital!
 
 app.Run();
